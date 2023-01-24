13580b35835f28d02be50db29ee1851da28bae4a
Awanthika Madhushani <100335819+awanthikamadhushani@users.noreply.github.com>
2023-01-24 21:43:41 +0530

    Update display.js

--- display.js
+++ display.js
@@ -1,13 +1,11 @@
 function displyuser(currentUser) {
 
   if(currentUser != undefined){
-      //console.log(Object.values(currentUser))
-
+    
       let userObj = Object.values(currentUser)[2].username
 
       console.log(userObj)
   }
-
 }
 
 module.exports.displyuser = displyuser
